d76e19d73629be34c0be6b4ad6113ce76c12ac36
Idba1 <moniraislamself@gmail.com>
2024-02-26 22:57:34 +0600

    object-operator

---
+++ object-literals.js
@@ -0,0 +1,9 @@
+function say(message,name){
+    return{
+        message,
+        name
+    }
+}
+console.log(say("hey hablu", "Ahmed"));
+console.log(say("hey programmer", "Eshan"));
+console.log(say("hello programmer", "Ahad"));

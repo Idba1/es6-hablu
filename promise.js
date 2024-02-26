91eb0b8181d25955cbadac45669728a0e23599cf
Idba1 <moniraislamself@gmail.com>
2024-02-27 03:38:00 +0600

    promise

---
+++ promise.js
@@ -0,0 +1,19 @@
+/* let message = false;
+
+let promise = new promise ((resolve,reject)=>{
+    if(message){
+        resolve([{
+            userName: "Eshan", email: "eshan@gmail.com"
+        }])
+    }
+    else{
+        reject("user is not valid");
+    }
+})
+
+promise.then((S)=>{
+    console.log(s);
+})
+promise.catch((error)=>{
+    console.log(error);
+}) */

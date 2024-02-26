80bfa1897e1ddab8e8a35bc1f5df8edfa57f1fb7
Idba1 <moniraislamself@gmail.com>
2024-02-26 23:13:17 +0600

    array-distructureing

---
+++ array-destructuring.js
@@ -0,0 +1,6 @@
+// let number = [1, 2, 4, 5, 7, 8, 6, 3, 6, 2]
+let number2 = [11, 22, 44, 55, 77, 88, 66, 33, 66, 22]
+
+let [a,b,c,d,e,f,...z] = number2;
+// console.log(z);
+console.log(a,b,c,d,e,f,...z)

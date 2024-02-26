24d3f9769f6b72f310d8966154384e6fd341f782
Idba1 <moniraislamself@gmail.com>
2024-02-26 23:40:01 +0600

    class-constructor

---
+++ class-constructor.js
@@ -0,0 +1,10 @@
+class Book {
+    constructor(bookName, price){
+        this.bookName = bookName;
+        this.price = price
+    }
+}
+let bookDetails = new Book("Opekkha", 300);
+console.log(bookDetails);
+let bookDetails2 = new Book("ei Shuvro", 550);
+console.log(bookDetails2)

724c181edcde58b4656ef53aacc0285220b07ee1
Idba1 <moniraislamself@gmail.com>
2024-02-27 03:30:15 +0600

    error-handling

---
+++ error-handling.js
@@ -0,0 +1,18 @@
+try {
+    console.log("hello");
+    console.log(num);
+    let age = 19;
+    if (age < 17) {
+        throw "hello baby";
+    }
+    else if (age > 18) {
+        throw "how are you?"
+    }
+}
+catch (hablu) {
+    console.log("how are you !")
+}
+finally {
+    console.log("hello hablu");
+}
+

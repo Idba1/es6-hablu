da58e45117823b36cf14ccbf105dc8c6a40e2cf2
Idba1 <moniraislamself@gmail.com>
2024-02-26 22:44:45 +0600

    var-let-const

---
+++ var-let-const.js
@@ -0,0 +1,8 @@
+/* 
+let – declare block-scoped variables using the let keyword.
+let vs. var – understand the differences between let and var.
+const – define constants using the const keyword.
+
+
+website: https://www.javascripttutorial.net/es6/
+*/
